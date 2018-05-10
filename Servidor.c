1cad2a589d091c26666d3f1d8f8e965134dc9a76
LeinB <38076042+LeinB@users.noreply.github.com>
2018-05-10 11:35:53 +0100

    Add files via upload
    Já lê o username e password de um ficheiro e adiciona a uma struct
    Tambem já compara o username da struct com o username inserido

--- Servidor.c
+++ Servidor.c
@@ -1,13 +1,40 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<stdlib.h>
 
 #define ANSI_COLOR_GREEN "\x1b[32m"
 #define ANSI_COLOR_RESET "\x1b[0m"
-int main(void){
 
+typedef struct{
+  char username[20];
+  char password[10];
+  int tipo;
+  }login;
+
+int carrega_utilizadores(login peepz[]){
+FILE *fx; char s[40],n=0,i,j=0;
+	fx=fopen("AdminLogin.txt","r");
+	while(fgets(s,40,fx)){
+		for(i=0;s[i]!=';';i++) peepz[n].username[i]=s[i];
+		peepz[n].username[i]='\0';
+		i++;
+		j=0;
+		for(;s[i]!=';';i++) peepz[n].password[j++]=s[i];
+		peepz[n].password[j]='\0';
+		i++;
+		j=0;
+                n++;
+	}
+	fclose(fx);
+	return n;
+}
+int main(void){
   int i = 0;
-  char USER[30];
-  FILE *ptr_file;
-  char buf [1000];
+  char User[20];
+  login peepz[5];
+  int n;
+  n = carrega_utilizadores(peepz);
   menu:
   printf(ANSI_COLOR_GREEN"**Menu de autenticação**" ANSI_COLOR_RESET "\n");
   printf("1) Login / autenticação \n");
@@ -25,16 +52,12 @@ int main(void){
   else{
   login:
     printf("Nome de utilizador:");
-    scanf("%s",USER);
-    ptr_file =fopen ("AdminLogin.txt","r");
-    while (fgets(buf,1000,ptr_file)!=NULL) {
-	if(buf==USER)
-	  break;
-      }
-    if(buf!=USER){
+    scanf("%s",User);
+    if(strcmp(User,peepz[0].username) != 0){
       printf("Utilizador inválido\n");
       printf("1)Voltar ao menu\n");
       printf("2)Tentar novamente\n");
+      printf("A sua Opção:");
       scanf("%d", &i);
       while(i <= 0 || i > 2){
 	printf("Opção não válida \n");
@@ -46,6 +69,8 @@ int main(void){
       if (i==2)
 	goto login;
     }
+    else{
+      return 0;
+    }
   } 
-}   
-	
+}
